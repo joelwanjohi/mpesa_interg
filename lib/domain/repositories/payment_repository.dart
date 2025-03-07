88fa5af6e065245bb4b878afa17f38fc9929613b
joelwanjohi <joelwanjohi207@gmail.com>
2025-03-07 20:37:22 +0300

    mpesa

--- lib/domain/repositories/payment_repository.dart
+++ lib/domain/repositories/payment_repository.dart
@@ -1,36 +1,36 @@
-import '../entities/payment.dart';
+// import '../entities/payment.dart';
 
-abstract class PaymentRepository {
-  // Initiate a payment transaction
-  Future<PaymentResult> initiatePayment(Payment payment);
-}
+// abstract class PaymentRepository {
+//   // Initiate a payment transaction
+//   Future<PaymentResult> initiatePayment(Payment payment);
+// }
 
-// Result class to encapsulate the outcome of a payment operation
-class PaymentResult {
-  final bool success;
-  final String message;
-  final Map<String, dynamic>? data;
+// // Result class to encapsulate the outcome of a payment operation
+// class PaymentResult {
+//   final bool success;
+//   final String message;
+//   final Map<String, dynamic>? data;
 
-  PaymentResult({
-    required this.success,
-    required this.message,
-    this.data,
-  });
+//   PaymentResult({
+//     required this.success,
+//     required this.message,
+//     this.data,
+//   });
 
-  // Factory constructor for success case
-  factory PaymentResult.success(String message, {Map<String, dynamic>? data}) {
-    return PaymentResult(
-      success: true,
-      message: message,
-      data: data,
-    );
-  }
+//   // Factory constructor for success case
+//   factory PaymentResult.success(String message, {Map<String, dynamic>? data}) {
+//     return PaymentResult(
+//       success: true,
+//       message: message,
+//       data: data,
+//     );
+//   }
 
-  // Factory constructor for failure case
-  factory PaymentResult.failure(String message) {
-    return PaymentResult(
-      success: false,
-      message: message,
-    );
-  }
-}
+//   // Factory constructor for failure case
+//   factory PaymentResult.failure(String message) {
+//     return PaymentResult(
+//       success: false,
+//       message: message,
+//     );
+//   }
+// }
