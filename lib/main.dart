8a651b08c81eb81abb2b84b8cebbed0ab1b6966d
joelwanjohi <joelwanjohi207@gmail.com>
2025-03-07 20:36:10 +0300

    mpesa

--- lib/main.dart
+++ lib/main.dart
@@ -227,44 +227,44 @@
 //   }
 // }
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:provider/provider.dart';
-// import 'core/di/injection_container.dart' as di;
-// import 'presentation/pages/payment_page.dart';
-// import 'presentation/providers/payment_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
+import 'core/di/injection_container.dart' as di;
+import 'presentation/pages/payment_page.dart';
+import 'presentation/providers/payment_provider.dart';
 
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   // Enable performance profiling if needed
-//   debugProfileBuildsEnabled = true;
-//   debugProfilePaintsEnabled = true;
+  // Enable performance profiling if needed
+  debugProfileBuildsEnabled = true;
+  debugProfilePaintsEnabled = true;
 
-//   // Initialize dependency injection
-//   di.init();
+  // Initialize dependency injection
+  di.init();
 
-//   runApp(const MyApp());
-// }
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(
-//           create: (_) => di.sl<PaymentProvider>(),
-//         ),
-//       ],
-//       child: MaterialApp(
-//         title: 'M-PESA Payment',
-//         theme: ThemeData(
-//           primarySwatch: Colors.green,
-//         ),
-//         home: const PaymentPage(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => di.sl<PaymentProvider>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'M-PESA Payment',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: const PaymentPage(),
+      ),
+    );
+  }
+}
